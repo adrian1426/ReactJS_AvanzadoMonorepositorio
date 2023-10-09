81365c78b7f9df59c6b7801cd07dfb5a89308ec1
adrianhh1426 <adrianhh1426@gmail.com>
2023-10-09 15:59:31 -0600

    playgroun react

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Button } from '@ahh/react';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Button label="mibtn" />
+);
